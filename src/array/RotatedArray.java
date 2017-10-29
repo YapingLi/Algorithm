c30eb0bd7edfb4ae49bd4e633ef6ed2a3de2f36b
Yaping Li <yapingbupt@gmail.com>
2017-10-29 16:16:29 -0400

    one algorithm

---
+++ src/array/RotatedArray.java
@@ -0,0 +1,9 @@
+public class RotatedArray {
+    /*
+    solution:
+    divide k by nums.length to get the actual rotated steps. Use an extra space to store the new array.
+    */
+    public void rotate(int[] nums, int k) {
+
+    }
+}
